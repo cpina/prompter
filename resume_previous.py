1104cce4d06f69d2500eb5ab3c94193e644b5bc7
Carles Pina i Estany <carles@pina.cat>
2023-09-19 23:59:26 +0100

    Add information word to start resuming

--- resume_previous.py
+++ resume_previous.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 
 from js import localStorage
 from pyscript import Element
@@ -7,7 +8,9 @@ def continue_previous():
     global current_status
 
     if previous_filename := localStorage.getItem("filename"):
-        Element("play_pause").write(f"Resume {previous_filename}")
+        number_of_words = len(json.loads(localStorage.getItem("lines")))
+        next_word = localStorage.getItem("next_word")
+        Element("play_pause").write(f"Resume {previous_filename} ({next_word}/{number_of_words})")
         Element("play_pause").element.style.display = "block"
         current_status = "resume"
     else:
